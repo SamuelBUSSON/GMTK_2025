79a8e0037e95c24e9e01e3f8c3645a3116308824
Samuel BUSSON <samuel.busson@hotmail.fr>
2025-08-02 12:43:57 +0200

    ✨ Cut feature First draft

--- Script/Game/hair.gd
+++ Script/Game/hair.gd
@@ -2,5 +2,26 @@ extends Node3D
 
 class_name hair;
 
-@export var bottom : Node;
-@export var top : Node;
+
+enum HairStyle { STRAIGHT, CURLY }
+enum HairSize  { SHORT, MEDIUM, LONG }
+
+@export_enum("SHORT", "MEDIUM", "LONG" ) var size: int;
+@export_enum("STRAIGHT", "CURLY") var style: int;
+# @export var mesh : Mesh;
+
+var hair_color : Color;
+
+
+func set_mesh_color(color : Color):
+	var mesh = $MeshInstance3D
+	var mat = mesh.get_active_material(0)
+	if mat == null:
+		mat = StandardMaterial3D.new()
+		mesh.set_surface_override_material(0, mat)
+	else:
+		mat = mat.duplicate()
+		mesh.set_surface_override_material(0, mat)
+
+	mat.albedo_color = color
+	hair_color = color;
