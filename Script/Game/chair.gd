17c9557de0c9aeaacf39deeef1248485fe55ba49
Samuel BUSSON <samuel.busson@hotmail.fr>
2025-08-03 15:27:08 +0200

    ✨ add mouse click spin

--- Script/Game/chair.gd
+++ Script/Game/chair.gd
@@ -1,9 +1,14 @@
 extends Node3D
 
 @export var spinning_speed: float = 1.0;
-
+@export var mouse_click_scale_speed: float = 4.0;
 
 func _process(dt):
 	if (GameGlobal.is_game_start):
-		self.rotate_y(spinning_speed * dt)
+
+		var speed = spinning_speed + GameGlobal.player_score * 0.5;
+		if Input.is_action_pressed("mouse_click_right"):
+			self.rotate_y(speed * dt * mouse_click_scale_speed)
+		else:
+			self.rotate_y(speed * dt)
 	pass
