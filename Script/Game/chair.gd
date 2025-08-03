9a2a997f8094c5cd994dd997040970b2c83350cf
Samuel BUSSON <samuel.busson@hotmail.fr>
2025-08-03 15:30:05 +0200

    ✨ export scale speed parameter

--- Script/Game/chair.gd
+++ Script/Game/chair.gd
@@ -2,11 +2,12 @@ extends Node3D
 
 @export var spinning_speed: float = 1.0;
 @export var mouse_click_scale_speed: float = 4.0;
+@export var scale_speed_based_on_score: float = 0.75;
 
 func _process(dt):
 	if (GameGlobal.is_game_start):
 
-		var speed = spinning_speed + GameGlobal.player_score * 0.5;
+		var speed = spinning_speed + GameGlobal.player_score * scale_speed_based_on_score;
 		if Input.is_action_pressed("mouse_click_right"):
 			self.rotate_y(speed * dt * mouse_click_scale_speed)
 		else:
